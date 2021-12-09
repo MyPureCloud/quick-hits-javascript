1a6eb6f79de35740ca2c5951625ab4cbccbd33bf
Prince Merluza <zero.bash@gmail.com>
2021-12-09 15:01:20 +0800

    DEVENGAGE-886 Add oauth-client-credentials

---
+++ authorization/oauth-client-credentials/oauth-client-credentials.js
@@ -0,0 +1,57 @@
+// >> START oauth-client-credentials Client credentials grant login without a client library. 
+
+const fetch = require('node-fetch');
+
+const clientId = process.env.GENESYS_CLOUD_CLIENT_ID;
+const clientSecret = process.env.GENESYS_CLOUD_CLIENT_SECRET;
+const environment = process.env.GENESYS_CLOUD_ENVIRONMENT; // expected format: mypurecloud.com
+
+// Test token by getting role definitions in the organization.
+function handleTokenCallback(body){
+    return fetch(`https://api.${environment}/api/v2/authorization/roles`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `${body.token_type} ${body.access_token}`
+        }
+    })
+    .then(res => {
+        if(res.ok){
+            return res.json();
+        } else {
+            throw Error(res.statusText);
+        }
+    })
+    .then(jsonResponse => {
+        console.log(jsonResponse);
+    })
+    .catch(e => console.error(e));
+}
+
+
+// Genesys Cloud Authentication
+const params = new URLSearchParams();
+params.append('grant_type', 'client_credentials');
+
+fetch(`https://login.${environment}/oauth/token`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Basic ${Buffer.from(clientId + ':' + clientSecret).toString('base64')}`
+    },
+    body: params
+})
+.then(res => {
+    if(res.ok){
+        return res.json();
+    } else {
+        throw Error(res.statusText);
+    }
+})
+.then(jsonResponse => {
+    console.log(jsonResponse);
+    handleTokenCallback(jsonResponse);
+})
+.catch(e => console.error(e));
+
+// >> END

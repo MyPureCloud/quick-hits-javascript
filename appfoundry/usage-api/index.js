936de3d2492608fd68de8ec010afd0d059b0165f
Prince Merluza <zero.bash@gmail.com>
2021-12-17 15:41:20 +0800

    Add premium app related guides

---
+++ appfoundry/usage-api/index.js
@@ -0,0 +1,116 @@
+// >> START usage-api This guide provides instructions and sample code for using the Premium Applications - Usage APIs
+const axios = require('axios');
+// >> START usage-api-step-1
+const apiKey = process.env.APIKEY;
+const appId = encodeURIComponent(process.env.APPID);
+// >> END usage-api-step-1
+// >> START usage-api-step-2
+/**
+ * Get summary of all regions and the number of orgs
+ * with the premium app.
+ * @returns {Promise}
+ */
+function getRegionSummaries() {
+    return axios.get(`https://billable-vendor-usage-api.usw2.pure.cloud/v1/regions?appIds=${appId}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': apiKey
+        }
+    })
+    .then((res) => {
+        let data = res.data;
+
+        console.log(`--- OVERALL CUSTOMER COUNT ---`);
+        console.log(data);
+
+        return data;
+    })
+    .catch(e => console.error(e));
+}
+// >> END usage-api-step-2
+// >> START usage-api-step-3
+/**
+ * Get summary of all organizations within a region
+ * and the usage quantity for each.
+ * @param {String} regionId eg. us-east-1
+ * @returns {Promise} Response from endpoint
+ */
+function getCustomersWithinRegion(regionId) {
+    return axios.get(`https://billable-vendor-usage-api.usw2.pure.cloud/v1/regions/${regionId}/organizations?appIds=${appId}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': apiKey
+        }
+    })
+    .then((res) => {
+        let data = res.data;
+
+        console.log(`--- CUSTOMERS FROM REGION ${regionId} ---`);
+        console.log(data);
+
+        return data;
+    })
+    .catch(e => console.error(e));
+}
+// >> END usage-api-step-3
+// >> START usage-api-step-4
+/**
+ * Prints the detailed info on a premium app subscription for a
+ * specific org.
+ * @param {String} regionId eg us-east-1
+ * @param {String} orgId Genesys Cloud org id
+ * @returns {Promise}
+ */
+function printOrgSubscription(regionId, orgId) {
+    return axios.get(`https://billable-vendor-usage-api.usw2.pure.cloud/v1/regions/${regionId}/organizations/${orgId}?appIds=${appId}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': apiKey
+        }
+    })
+    .then((res) => {
+        let data = res.data;
+
+        console.log(`--- SUBSCRIPTION FROM REGION ${regionId}, FROM ORG ${orgId} ---`);
+        console.log(data);
+
+        return data;
+    })
+    .catch(e => console.error(e));
+}
+// >> END usage-api-step-4
+
+// Run all the functions in series.
+// (Will print all subscription info of all orgs from all regions)
+getRegionSummaries()
+.then((data) => {
+    let regionPromisesArr = [];
+
+    // Get all customers through all the regions
+    for (let region of data.regions) {
+        regionPromisesArr.push(getCustomersWithinRegion(region.id));
+    }
+
+    return Promise.all(regionPromisesArr);
+})
+.then((allRegionsData) => {
+    let allOrgsPromiseArr = [];
+
+    // Go customer orgs per region
+    for(let regionData of allRegionsData){
+        let organizations = regionData.organizations;
+
+        // Get subscription info from the org
+        organizations.forEach(org => {
+            allOrgsPromiseArr.push(printOrgSubscription(regionId, org.orgId));
+        })
+    }   
+    
+    return Promise.all(allOrgsPromiseArr);
+})
+.then(() => {
+    // After all org subscription has been printed
+    console.log('--- DONE ---');
+})
+.catch((e) => console.error(e));
+// >> END usage-api

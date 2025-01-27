44b7bc00f85085ada8c8460405208282c833ec40
Jerome Saint-Marc <jerome.saint-marc@genesys.com>
2025-01-27 14:53:51 +0100

    Genesys Functions: Alternative to Serverless Framework to package and test function

---
+++ genesys-functions/jwe-encrypt-decrypt/encrypt-decrypt-with-status/test/test_function_ts.mjs
@@ -0,0 +1,12 @@
+import { handler } from '../function_ts/dist/index.js';
+import { default as fs } from 'fs';
+try {
+    console.log(JSON.stringify(
+        await handler(
+            JSON.parse(fs.readFileSync(process.argv[2])),
+            JSON.parse(fs.readFileSync(process.argv[3])),
+            function (data, result) { if (data !== null && data !== undefined) { throw data; } else { if (result !== null && result !== undefined) console.log(JSON.stringify(result, null, 4)); } }
+        ), null, 4));
+} catch (err) {
+    console.error(JSON.stringify({ errorMessage: err.message, errorType: err.name, stackTrace: err.stack }, null, 4));
+}
